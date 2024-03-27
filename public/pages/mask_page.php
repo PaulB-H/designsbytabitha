0cd3b31066ffe550ab81f74a5b6361b7e14a9054
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2024-03-27 08:30:49 -0400

    Remove google analytics from mask page

--- public/pages/mask_page.php
+++ public/pages/mask_page.php
@@ -4,20 +4,6 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=UA-180184444-1"
-    ></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-
-      gtag("config", "UA-180184444-1");
-    </script>
 
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
