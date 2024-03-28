dfb5c8cc7443e82012ba51de71cfdff0752c5d25
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2024-03-28 17:37:18 -0400

    Update to use PDO & a table join

--- public/php/admin_fetchDetails.php
+++ public/php/admin_fetchDetails.php
@@ -1,54 +1,79 @@
 <?php
 
-  include("./session_start.php");
+include("./session_start.php");
 
-  if ($_SESSION["roles"] !== "admin") {
-    echo(JSON_encode("No Access"));
-  } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include("./config_ordersDB.php");
+if(!isset($_SESSION["roles"]) || $_SESSION["roles"] !== "admin"){
+  http_response_code(403); 
+  echo(json_encode("No Access"));
+  exit();
+}
 
-    // Incomming - Single order number
-    $json = file_get_contents('php://input');
-    $data = json_decode($json);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  include("../../pdo_config.php");
 
-    $return_arr = array();
+  $json = file_get_contents('php://input');
+  $data = json_decode($json);
 
-    $query = "SELECT * FROM `orders` WHERE OrderNum = $data ";
-    $result = mysqli_query($con,$query);
-
-    while($row = mysqli_fetch_array($result)){
-      $orderNum = $row['OrderNum'];
-      $Date = $row['Date'];
-      $Email = $row['Email'];
-      $orderStatus = $row['OrderStatus'];
-
-      $return_arr[] = array(
-        "orderNum" => $orderNum,
-        "Date" => $Date,
-        "Email" => $Email,
-        "orderStatus" => $orderStatus,
-      );
-    }
-
-    $query = "SELECT * FROM `order_items` WHERE OrderNum = $data ";
-    $result = mysqli_query($con,$query);
-
-    while($row = mysqli_fetch_array($result)){
-      $item = $row['Item'];
-      $size = $row['Size'];
-      $qnty = $row['Qnty'];
-      $made = $row['Made'];
-
-      $return_arr[] = array(
-        "item" => $item,
-        "size" => $size,
-        "qnty" => $qnty,
-        "made" => $made,
-      );
-    }
-
-    // echo json_encode($data);
-    echo json_encode($return_arr);
+  if (!is_int($data)) {
+    http_response_code(400);
+    echo(json_encode(['error' => 'Number only']));
+    exit();
   }
+
+  try {
+    $query = "SELECT orders.*, order_items.*
+              FROM `orders`
+              LEFT JOIN `order_items` ON orders.OrderNum = order_items.OrderNum
+              WHERE orders.OrderNum = ?";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(1, $data, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $orderDetails = null;
+    $orderItems = array();
+
+    if (count($rows) === 0) { 
+      echo(json_encode(['error' => 'No orders found']));
+      exit();
+    }
+
+    foreach ($rows as $row) {
+      // First row will be the order details
+      if ($orderDetails === null) {
+          $orderDetails = array(
+            'OrderNum' => $row['OrderNum'],
+            'Date' => $row['Date'],
+            'Email' => $row['Email'],
+            'OrderStatus' => $row['OrderStatus'],
+            'orderItems' => array(),
+          );
+      }
+
+      // This creates an "associative array" which is like a dictionary or map,
+      // or like a traditional JS object: 
+      // { 
+      //   'Item': $row['Item'], 
+      //   'Size': $row['Size'], 
+      //   'Qnty': $row['Qnty'], 
+      //   'Made': $row['Made']
+      // } 
+      $orderItems[] = array(
+        'Item' => $row['Item'],
+        'Size' => $row['Size'],
+        'Qnty' => $row['Qnty'],
+        'Made' => $row['Made'],
+      );
+    }
+
+    $orderDetails['orderItems'] = $orderItems;
+
+    echo json_encode($orderDetails);
+  } catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(["error" => "DB Error fetching order details"]);
+  }
+}
 
 ?>
