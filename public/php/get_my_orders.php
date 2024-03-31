171695eeee23baf3e96645b17e153667d455fa61
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2024-03-31 05:22:45 -0400

    Update to use PDO

--- public/php/get_my_orders.php
+++ public/php/get_my_orders.php
@@ -1,34 +1,43 @@
 <?php
 
-  include("./session_start.php");
+include("./session_start.php");
 
-  if(!$_SESSION["user"]){
-    echo JSON_encode("You must be logged in to view your orders");
-  } else {
-    include("./config_ordersDB.php");
+if(!isset($_SESSION["user"]) || !isset($_SESSION["email"]) ){
+  http_response_code(403); 
+  echo json_encode("You must be logged in to view your orders");
+  exit();
+}
 
-    $return_arr = array();
-    $email = $_SESSION["email"];
-    
-    $query = "SELECT * FROM orders WHERE Email = '$email' ";
-    $result = mysqli_query($con,$query);
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+  include("../../pdo_config.php");
 
-    while($row = mysqli_fetch_array($result)){
-      $orderNum = $row['OrderNum'];
-      $Date = $row['Date'];
-      $Email = $row['Email'];
-      $orderStatus = $row['OrderStatus'];
+  $return_arr = array();
 
-      $return_arr[] = array(
-        "orderNum" => $orderNum,
-        "Date" => $Date,
-        "Email" => $Email,
-        "orderStatus" => $orderStatus,
-      );
-    }
+  $email = $_SESSION["email"];
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    http_response_code(400);
+    echo json_encode("Invalid email format");
+    exit; 
+  }
+
+  $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+  try {
+
+    $query = "SELECT * FROM orders WHERE Email = :email";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $return_arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     echo json_encode($return_arr);
-
+  } catch (PDOException $e) {
+    // echo json_encode("Error: " . $e->getMessage());
+    http_response_code(500);
+    echo json_encode(['error' => 'DB Error']);
   }
+}
 
 ?>
